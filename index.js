c047acdf7b1b315a33272efbc2bd5bb7fe2a0c67
James <jameskimanzi@gmail.com>
2024-01-21 19:55:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+let main = document.getElementById ('main');
+
+// console.log(main);
+
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = 'JAMES is the champion';
+
+document.body.append(newHeader);
+
+
+
